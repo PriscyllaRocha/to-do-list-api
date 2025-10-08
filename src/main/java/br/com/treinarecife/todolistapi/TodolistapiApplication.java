3badc639836708bdcd845c6179e0a3a904024d7b
Priscylla Rocha <priscyllarocha104@gmail.com>
2025-10-08 16:12:19 -0300

    Config: Permite CORS para frontend React (localhost:5173)

--- src/main/java/br/com/treinarecife/todolistapi/TodolistapiApplication.java
+++ src/main/java/br/com/treinarecife/todolistapi/TodolistapiApplication.java
@@ -2,12 +2,26 @@ package br.com.treinarecife.todolistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TodolistapiApplication {
+public class ToDoListApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodolistapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApiApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // endereço do Vite/React
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
